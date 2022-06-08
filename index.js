fd291462c1fd0c3e715e619bb083efffe12559cd
rahimedu <edwin.duche82@gmail.com>
2022-06-08 00:38:55 -0400

    message

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+
+const num1 =2;
+const num2 =31;
+const multiply = num1 * num2;
+
+
+const random =Math.floor (Math.random()* 12) + 2;
+
+const num3 = 20;
+const num4 = 8;
+const mod = num3 % num4;
+
+const max = Math.max(3,6,12,15,18,20);
